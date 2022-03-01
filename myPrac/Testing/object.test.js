8dcd37eccc0771de7e5c92a696f4ddb87f226aba
noellamwanzia <39863136+noellamwanzia@users.noreply.github.com>
2022-03-01 12:27:21 +0300

    :+1: update js text file

---
+++ myPrac/Testing/object.test.js
@@ -0,0 +1,5 @@
+test('obj assignment',()=>{
+    const data = {one: 1}
+    data['two']=6
+    expect(data).toEqual({one:1,two:2})
+})
---
+++ myPrac/Testing/sum.test.js
@@ -0,0 +1,4 @@
+const sum = require('./sum');
+test('test 2 plus 8  to be 10',()=>{
+expect(sum(6,8)).toBe(14);
+})
